5a5e9eb037fe34062df266912a78f83e84f75c08
Charlie Gleason <hi@superhighfives.com>
2012-09-23 22:46:46 +1000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+group :development, :test do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg' # this gem is required to use postgres on Heroku
+end
+
+gem 'compass'
+gem 'sinatra'
+gem 'sinatra-activerecord'
+gem 'haml'
+gem 'sass'
+gem 'maruku'
---
+++ app.rb
@@ -0,0 +1,41 @@
+# app.rb
+require 'compass'
+require 'sinatra'
+require 'sinatra/activerecord'
+require './db/models'
+require 'haml'
+require 'sass'
+require 'maruku'
+require 'uri'
+
+configure :development, :test do
+  set :database, 'sqlite3:///the41against.db'
+end
+
+configure :production do
+  # Database connection
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/the41against')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
+
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
+end
+
+get '/' do
+  @politicians = Politician.all
+  haml :index
+end
+
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options)
+end
